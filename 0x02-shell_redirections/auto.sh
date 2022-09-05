13cda63abf25c76764f9f0698b5ff44c01cb1fb0
Sunney1431 <cokersunday36@gmail.com>
2022-09-05 15:12:43 -0700

    auto.sh

---
+++ 0x02-shell_redirections/auto.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+
+
+ls > alt
+
+LINES=$(cat alt)
+
+
+
+for LINE in $LINES
+	    
+do
+    
+    if [ "$LINE" = 'auto.sh' ]; then
+	
+	echo "$LINE"
+	
+        git add $LINE
+	
+        git commit -m "$LINE"
+	
+        git push
+	
+    else
+	
+	echo "At auto.sh"	
+    fi
+    
+
+    
+
+    
+done
